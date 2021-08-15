71f4470457052efddcd45c03188ff6bf0c1d0a60
Trey98 <78447988+Trey98@users.noreply.github.com>
2021-08-15 16:44:33 -0500

    crab
    crab

--- main/java/com/rainbowdestiny/konchu/main/init/ItemInit.java
+++ main/java/com/rainbowdestiny/konchu/main/init/ItemInit.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.RegistryObject;
 public class ItemInit {
 
 	
-	//Items
+	//Tool Items
 	public static final RegistryObject<Item> WOODEN_NET = KonchuRegistry.ITEMS.register("wooden_net", () -> new NetItem(25, ItemTier.WOOD));
 	public static final RegistryObject<Item> CLOTH_NET = KonchuRegistry.ITEMS.register("cloth_net", () -> new NetItem(30, ItemTier.STONE));
 	public static final RegistryObject<Item> GOLDEN_NET = KonchuRegistry.ITEMS.register("golden_net", () -> new NetItem(80, ItemTier.GOLD));
@@ -19,11 +19,14 @@ public class ItemInit {
 	public static final RegistryObject<Item> DIAMOND_NET = KonchuRegistry.ITEMS.register("diamond_net", () -> new NetItem(70, ItemTier.DIAMOND));
 	public static final RegistryObject<Item> NETHERITE_NET = KonchuRegistry.ITEMS.register("netherite_net", () -> new NetItem(85, ItemTier.NETHERITE));
 	
+	//Item Items
 	public static final RegistryObject<Item> NET = KonchuRegistry.ITEMS.register("net", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
 	public static final RegistryObject<Item> LICHEN_CLUMP = KonchuRegistry.ITEMS.register("lichen_clump", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+	public static final RegistryObject<Item> CRAB_SHELL = KonchuRegistry.ITEMS.register("crab_shell",() -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
 
 	//Block Items
     public static final RegistryObject<Item> LICHEN_BLOCK_ITEM = KonchuRegistry.ITEMS.register("lichen_block", () -> new ItemBase(BlockInit.LICHEN_BLOCK.get(),ItemGroup.TAB_DECORATIONS));
     public static final RegistryObject<Item> CRAB_BLOCK_ITEM = KonchuRegistry.ITEMS.register("crab_block", () -> new ItemBase(BlockInit.CRAB_BLOCK.get(), ItemGroup.TAB_BUILDING_BLOCKS));
+   
 
 }
